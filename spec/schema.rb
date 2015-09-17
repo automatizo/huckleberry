ae8124c5d71533414a2c898ab41c470aec7c32ef
Justin Kuepper <justin.kuepper@gmail.com>
2015-09-17 10:58:27 -0600

    added carb spec

--- spec/schema.rb
+++ spec/schema.rb
@@ -30,4 +30,16 @@ ActiveRecord::Schema.define do
     t.float :omega_6_4
     t.float :omega_6_5
   end
+
+  create_table :huckleberry_carbohydrates do |t|
+    t.string :nutrient_databank_number, null: false, index: true, unique: true
+    t.float :total_carbohydrate
+    t.float :dietary_fiber
+    t.float :starch
+    t.float :sugar
+    t.float :glucose
+    t.float :sucrose
+    t.float :fructose
+    t.float :lactose
+  end
 end
