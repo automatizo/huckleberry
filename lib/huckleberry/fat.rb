450f2510716301c8ef70058e268c86c19242fa06
Justin Kuepper <justin.kuepper@gmail.com>
2015-09-22 10:27:30 -0600

    removed test method

--- lib/huckleberry/fat.rb
+++ lib/huckleberry/fat.rb
@@ -5,9 +5,5 @@ module Huckleberry
     validates :nutrient_databank_number, presence: true, uniqueness: { allow_blank: true }
 
     belongs_to :ingredient, foreign_key: :nutrient_databank_number
-
-    def omega3
-      puts "Works"
-    end
   end
 end
